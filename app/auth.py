bbce25d15ef9846297149e46ac7831fd3a741aea
David Front <dfront@gmail.com>
2025-05-08 14:21:37 +0300

    rm depricated app/auth.py

--- app/auth.py
+++
@@ -1,5 +0,0 @@
-# app/auth.py
-# This file is deprecated. Please use app/auth/router.py instead.
-# Keeping this file for backward compatibility.
-
-from app.auth.router import auth_router, register_router
